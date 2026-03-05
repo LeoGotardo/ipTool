dbdbb3ed527ebe4f77e26b8634305bd10c74d0fe
Leonardo Gotardo <125305856+LeoGotardo@users.noreply.github.com>
2026-03-05 13:56:03 -0300

    Update page.jsx

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -12,6 +12,7 @@ export function Page() {
   return (
     <div className="page">
       <h1>IPTool lookup</h1>
+        <link rel="icon" href="/favicon.ico" />
         <div className="input-container"><Input tyle={"text"} placeholder={"IP"} id={"ip"} /></div>
         <div className="button-container"><Button callback={lookup} label={"Lookup"} /></div>
         <div className="response-container"><Response /></div>
